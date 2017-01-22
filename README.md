50ac29492cb0731ea467ec062c74ac0e6b5f09f2
daddinuz <daddinuz@gmail.com>
2017-01-22 22:14:17 +0100

    mega refactor

--- README.md
+++ README.md
@@ -7,31 +7,61 @@ Watchdog also provides some basic garbage collection utilities.
 
 ```sh
 $ ./bin/example 
-[MEMORY] DUMP: 3 allocations, 1 reallocations, 1 frees
-[MEMORY]          at 0xc04278 in (example.c:51):  10 bytes allocated
-[MEMORY]          at 0xc042b8 in (example.c:47):   8 bytes allocated
-[MEMORY]       HEAP: 18 bytes allocated
-[MEMORY] WARN: running garbage collection
-[MEMORY]          at 0xc04278 in (example.c:51):  10 bytes allocated
-[MEMORY]          at 0xc042b8 in (example.c:47):   8 bytes allocated
-[MEMORY]       HEAP: 18 bytes collected
+[WATCHDOG] DUMP: 3 allocations, 1 reallocations, 1 frees
+[WATCHDOG]       address 0x189d298:
+[WATCHDOG]            malloc     at (/home/daddinuz/Workspace/C/Watchdog/examples/example.c:46):    6 bytes allocated
+[WATCHDOG]            realloc    at (/home/daddinuz/Workspace/C/Watchdog/examples/example.c:51):   10 bytes allocated
+[WATCHDOG]       address 0x189d388:
+[WATCHDOG]            calloc     at (/home/daddinuz/Workspace/C/Watchdog/examples/example.c:47):    8 bytes allocated
+[WATCHDOG]       address 0x189d4e8:
+[WATCHDOG]            calloc     at (/home/daddinuz/Workspace/C/Watchdog/examples/example.c:55):   20 bytes allocated
+[WATCHDOG]            free       at (/home/daddinuz/Workspace/C/Watchdog/examples/example.c:56):    0 bytes allocated
+[WATCHDOG]       ---- 18 bytes currently allocated
+[WATCHDOG] WARN: running garbage collection
+[WATCHDOG]       address 0x189d298:
+[WATCHDOG]            collecting  10 bytes
+[WATCHDOG]       address 0x189d388:
+[WATCHDOG]            collecting   8 bytes
+[WATCHDOG]       address 0x189d4e8:
+[WATCHDOG]            collecting   0 bytes
+[WATCHDOG]       ---- 18 bytes collected
 ```
 
-Example dump file
+In the dump file under bin/ folder
 
 ```
-[MEMORY] DUMP: 0 allocations, 0 reallocations, 0 frees
-[MEMORY]       HEAP: 0 bytes allocated
-[MEMORY] DUMP: 2 allocations, 0 reallocations, 0 frees
-[MEMORY]          at 0xc042b8 in (example.c:47):   8 bytes allocated
-[MEMORY]          at 0xc04278 in (example.c:46):   6 bytes allocated
-[MEMORY]       HEAP: 14 bytes allocated
-[MEMORY] DUMP: 2 allocations, 1 reallocations, 0 frees
-[MEMORY]          at 0xc04278 in (example.c:51):  10 bytes allocated
-[MEMORY]          at 0xc042b8 in (example.c:47):   8 bytes allocated
-[MEMORY]       HEAP: 18 bytes allocated
-[MEMORY] DUMP: 3 allocations, 1 reallocations, 1 frees
-[MEMORY]          at 0xc04278 in (example.c:51):  10 bytes allocated
-[MEMORY]          at 0xc042b8 in (example.c:47):   8 bytes allocated
-[MEMORY]       HEAP: 18 bytes allocated
+[WATCHDOG] DUMP: 0 allocations, 0 reallocations, 0 frees
+[WATCHDOG]       ---- 0 bytes currently allocated
+[WATCHDOG] CALL: at (/home/daddinuz/Workspace/C/Watchdog/examples/example.c:46) -- malloc  : 6 bytes requested
+[WATCHDOG] DONE: at (/home/daddinuz/Workspace/C/Watchdog/examples/example.c:46) -- malloc  : 6 bytes allocated to address 0x189d298
+[WATCHDOG] CALL: at (/home/daddinuz/Workspace/C/Watchdog/examples/example.c:47) -- calloc  : 8 bytes requested
+[WATCHDOG] DONE: at (/home/daddinuz/Workspace/C/Watchdog/examples/example.c:47) -- calloc  : 8 bytes allocated to address 0x189d388
+[WATCHDOG] DUMP: 2 allocations, 0 reallocations, 0 frees
+[WATCHDOG]       address 0x189d298:
+[WATCHDOG]            malloc     at (/home/daddinuz/Workspace/C/Watchdog/examples/example.c:46):    6 bytes allocated
+[WATCHDOG]       address 0x189d388:
+[WATCHDOG]            calloc     at (/home/daddinuz/Workspace/C/Watchdog/examples/example.c:47):    8 bytes allocated
+[WATCHDOG]       ---- 14 bytes currently allocated
+[WATCHDOG] CALL: at (/home/daddinuz/Workspace/C/Watchdog/examples/example.c:51) -- realloc : 10 bytes requested
+[WATCHDOG] DONE: at (/home/daddinuz/Workspace/C/Watchdog/examples/example.c:51) -- realloc : 10 bytes reallocated from address 0x189d298 to address 0x189d298
+[WATCHDOG] DUMP: 2 allocations, 1 reallocations, 0 frees
+[WATCHDOG]       address 0x189d298:
+[WATCHDOG]            malloc     at (/home/daddinuz/Workspace/C/Watchdog/examples/example.c:46):    6 bytes allocated
+[WATCHDOG]            realloc    at (/home/daddinuz/Workspace/C/Watchdog/examples/example.c:51):   10 bytes allocated
+[WATCHDOG]       address 0x189d388:
+[WATCHDOG]            calloc     at (/home/daddinuz/Workspace/C/Watchdog/examples/example.c:47):    8 bytes allocated
+[WATCHDOG]       ---- 18 bytes currently allocated
+[WATCHDOG] CALL: at (/home/daddinuz/Workspace/C/Watchdog/examples/example.c:55) -- calloc  : 20 bytes requested
+[WATCHDOG] DONE: at (/home/daddinuz/Workspace/C/Watchdog/examples/example.c:55) -- calloc  : 20 bytes allocated to address 0x189d4e8
+[WATCHDOG] CALL: at (/home/daddinuz/Workspace/C/Watchdog/examples/example.c:56) -- free    : address 0x189d4e8
+[WATCHDOG] DUMP: 3 allocations, 1 reallocations, 1 frees
+[WATCHDOG]       address 0x189d298:
+[WATCHDOG]            malloc     at (/home/daddinuz/Workspace/C/Watchdog/examples/example.c:46):    6 bytes allocated
+[WATCHDOG]            realloc    at (/home/daddinuz/Workspace/C/Watchdog/examples/example.c:51):   10 bytes allocated
+[WATCHDOG]       address 0x189d388:
+[WATCHDOG]            calloc     at (/home/daddinuz/Workspace/C/Watchdog/examples/example.c:47):    8 bytes allocated
+[WATCHDOG]       address 0x189d4e8:
+[WATCHDOG]            calloc     at (/home/daddinuz/Workspace/C/Watchdog/examples/example.c:55):   20 bytes allocated
+[WATCHDOG]            free       at (/home/daddinuz/Workspace/C/Watchdog/examples/example.c:56):    0 bytes allocated
+[WATCHDOG]       ---- 18 bytes currently allocated
 ```
--- src/watchdog.c
+++ src/watchdog.c
@@ -37,13 +37,19 @@ typedef struct memory_trace_t {
     size_t size;
 } memory_trace_t;
 
+typedef struct memory_info_t {
+    XorList_t *trace_list;
+    void *address;
+    bool allocated;
+} memory_info_t;
+
 typedef struct memory_chunk_t {
-    XorList_t *history;
+    memory_info_t *info;
 } memory_chunk_t;
 
-static XorList_t *__memory = NULL;
-static size_t __allocations = 0, __reallocations = 0, __frees = 0;
 static FILE *__stream = NULL;
+static XorList_t *__info_list = NULL, *__chunk_list = NULL;
+static size_t __allocations = 0, __reallocations = 0, __frees = 0;
 
 /* Check if a stream is set otherwise redirect to stderr */
 #define STREAM ((NULL != __stream) ? __stream : stderr)
@@ -51,63 +57,69 @@ static FILE *__stream = NULL;
 /*
  * Internal functions
  */
-static memory_chunk_t *__watchdog_allocate(size_t size, char *file, size_t line, bool clear) {
-    memory_chunk_t *new_chunk = malloc(sizeof(memory_chunk_t) + size);
+static void *__watchdog_allocate(size_t size, char *file, size_t line, bool clear) {
+    memory_chunk_t *chunk = malloc(sizeof(memory_chunk_t) + size);
     /** Do not try to handle allocation errors, malloc/calloc behaviour must not be altered **/
-    if (NULL == new_chunk) {
-        fprintf(STREAM, "[MEMORY] FAIL: %s(%zu) in (%s:%zu)\n", clear ? "calloc" : "malloc", size, file, line);
+    if (NULL == chunk) {
         return NULL;
     }
-    new_chunk->history = xor_list_new();
-    __allocations += 1;
-    if (true == clear) {
-        memset(new_chunk + 1, 0, size);
+    if (clear) {
+        memset(chunk + 1, 0, size);
     }
+    __allocations += 1;
+    xor_list_push_back(__chunk_list, chunk);
+    memory_info_t *info = malloc(sizeof(memory_info_t));
+    assert(NULL != info);
+    info->trace_list = xor_list_new();
+    info->address = (chunk + 1);
+    info->allocated = true;
     memory_trace_t *trace = malloc(sizeof(memory_trace_t));
     assert(NULL != trace);
     trace->action = clear ? "calloc" : "malloc";
     trace->file = file;
     trace->line = line;
     trace->size = size;
-    xor_list_push_back(new_chunk->history, trace);
-    return new_chunk;
+    xor_list_push_back(info->trace_list, trace);
+    xor_list_push_back(__info_list, info);
+    chunk->info = info;
+    return chunk + 1;
 }
 
-static memory_chunk_t *__watchdog_reallocate(void *ptr, size_t size, char *file, size_t line) {
+static void *__watchdog_reallocate(void *ptr, size_t size, char *file, size_t line) {
     memory_chunk_t *chunk = (memory_chunk_t *) ptr - 1;
+    memory_info_t *info = chunk->info;
     /** Do not try to handle realloc errors, realloc behaviour must not be altered **/
-    if (NULL == realloc(chunk, sizeof(memory_chunk_t) + size)) {
-        fprintf(STREAM, "[MEMORY] FAIL: realloc(%p, %zu) in (%s:%zu)\n", ptr, size, file, line);
+    if (NULL == (chunk = realloc(chunk, sizeof(memory_chunk_t) + size))) {
         return NULL;
     }
     __reallocations += 1;
+    xor_list_push_back(__chunk_list, chunk);
+    chunk->info = info;
+    info->address = (chunk + 1);
+    info->allocated = true;
     memory_trace_t *trace = malloc(sizeof(memory_trace_t));
     assert(NULL != trace);
     trace->action = "realloc";
     trace->file = file;
     trace->line = line;
     trace->size = size;
-    xor_list_push_back(chunk->history, trace);
-    return chunk;
+    xor_list_push_back(info->trace_list, trace);
+    return chunk + 1;
 }
 
 static void __watchdog_free(void *ptr, char *file, size_t line) {
     memory_chunk_t *chunk = (memory_chunk_t *) ptr - 1;
-    /*
-     * C standard, 7.20.3.2/2 from ISO-IEC 9899: 
-     * If ptr is a null pointer, no action occurs.
-     */
-    if (NULL != ptr) {
-        assert(NULL != realloc(chunk, sizeof(memory_chunk_t)));
-    }
-    __frees += 1;
+    memory_info_t *info = chunk->info;
     memory_trace_t *trace = malloc(sizeof(memory_trace_t));
     assert(NULL != trace);
     trace->action = "free";
     trace->file = file;
     trace->line = line;
     trace->size = 0;
-    xor_list_push_back(chunk->history, trace);
+    info->allocated = false;
+    xor_list_push_back(info->trace_list, trace);
+    free(chunk);
+    __frees += 1;
 }
 
 /* If track is enabled log the call on the appropriate stream else do nothing */
@@ -120,22 +132,24 @@ static void __watchdog_free(void *ptr, char *file, size_t line) {
  * Public functions
  */
 void watchdog_initialize(void) {
-    __memory = xor_list_new();
+    __info_list = xor_list_new();
+    __chunk_list = xor_list_new();
 }
 
 void watchdog_terminate(void) {
     memory_trace_t *current_trace = NULL;
-    memory_chunk_t *current_chunk = NULL;
-    for (size_t c = xor_list_size(__memory); c > 0 ; c--) {
-        current_chunk = xor_list_pop_back(__memory);
-        for (size_t t = xor_list_size(current_chunk->history); t > 0; t--) {
-            current_trace = xor_list_pop_back(current_chunk->history);
+    memory_info_t *current_info = NULL;
+    for (size_t i = xor_list_size(__info_list); i > 0; i--) {
+        current_info = xor_list_pop_back(__info_list);
+        for (size_t t = xor_list_size(current_info->trace_list); t > 0; t--) {
+            current_trace = xor_list_pop_back(current_info->trace_list);
             free(current_trace);
         }
-        xor_list_delete(current_chunk->history);
-        free(current_chunk);
+        xor_list_delete(current_info->trace_list);
+        free(current_info);
     }
-    xor_list_delete(__memory);
+    xor_list_delete(__info_list);
+    xor_list_delete(__chunk_list);
 }
 
 void watchdog_set_output_stream(FILE *stream) {
@@ -145,84 +159,90 @@ void watchdog_set_output_stream(FILE *stream) {
 void watchdog_dump(void) {
     size_t bytes_allocated = 0;
     memory_trace_t *current_trace = NULL;
-    memory_chunk_t *current_chunk = NULL;
-    fprintf(STREAM, "[MEMORY] DUMP: %zu allocations, %zu reallocations, %zu frees\n",
+    memory_info_t *current_info = NULL;
+    fprintf(STREAM, "[WATCHDOG] DUMP: %zu allocations, %zu reallocations, %zu frees\n",
             __allocations, __reallocations, __frees);
-    for (size_t c = 0; c < xor_list_size(__memory); c++) {
-        current_chunk = xor_list_get(__memory, c);
-        fprintf(STREAM, "[MEMORY] address %p:\n", (void *) (current_chunk + 1));
-        for (size_t t = 0; t < xor_list_size(current_chunk->history); t++) {
-            current_trace = xor_list_get(current_chunk->history, t);
-            fprintf(STREAM, "[MEMORY] %*c %s at (%s:%zu): %4zu bytes allocated\n",
-                    8, ' ', current_trace->action, current_trace->file, current_trace->line, current_trace->size
+    for (size_t c = 0; c < xor_list_size(__info_list); c++) {
+        current_info = xor_list_get(__info_list, c);
+        fprintf(STREAM, "[WATCHDOG] %-5s address %p:\n", "", current_info->address);
+        for (size_t t = 0; t < xor_list_size(current_info->trace_list); t++) {
+            current_trace = xor_list_get(current_info->trace_list, t);
+            fprintf(STREAM, "[WATCHDOG] %-10s %-10s at (%s:%zu): %4zu bytes allocated\n", "",
+                    current_trace->action, current_trace->file, current_trace->line, current_trace->size
             );
         }
-        bytes_allocated += ((memory_trace_t *) xor_list_back(current_chunk->history))->size;
+        bytes_allocated += ((memory_trace_t *) xor_list_back(current_info->trace_list))->size;
     }
-    fprintf(STREAM, "[MEMORY] %*c HEAP: %zu bytes currently allocated\n", 5, ' ', bytes_allocated);
+    fprintf(STREAM, "[WATCHDOG] %-5s ---- %zu bytes currently allocated\n", "", bytes_allocated);
 }
 
 void watchdog_collect(void) {
     size_t bytes_collected = 0;
     memory_trace_t *current_trace = NULL;
-    memory_chunk_t *current_chunk = NULL;
-    fprintf(STREAM, "[MEMORY] WARN: running garbage collection\n");
-    for (size_t c = 0; c < xor_list_size(__memory); c++) {
-        current_chunk = xor_list_get(__memory, c);
-        fprintf(STREAM, "[MEMORY] address %p:\n", (void *) (current_chunk + 1));
-        current_trace = ((memory_trace_t *) xor_list_back(current_chunk->history));
+    memory_info_t *current_info = NULL;
+    fprintf(STREAM, "[WATCHDOG] WARN: running garbage collection\n");
+    for (size_t c = 0; c < xor_list_size(__info_list); c++) {
+        current_info = xor_list_get(__info_list, c);
+        fprintf(STREAM, "[WATCHDOG] %-5s address %p:\n", "", current_info->address);
+        current_trace = ((memory_trace_t *) xor_list_back(current_info->trace_list));
         bytes_collected += current_trace->size;
-        fprintf(STREAM, "[MEMORY] %*c collecting %3zu bytes\n",
-                8, ' ', current_trace->size
+        fprintf(STREAM, "[WATCHDOG] %-10s collecting %3zu bytes\n", "", current_trace->size
         );
-        __watchdog_free(current_chunk + 1, "<garbage collector>", 0);
+        if (current_info->allocated) {
+            __watchdog_free(current_info->address, "<garbage collector>", 0);
+        }
     }
-    fprintf(STREAM, "[MEMORY] %*c HEAP: %zu bytes collected\n", 5, ' ', bytes_collected);
+    fprintf(STREAM, "[WATCHDOG] %-5s ---- %zu bytes collected\n", "", bytes_collected);
 }
 
 /*
  * Protected functions
  */
 void *watchdog_malloc(size_t size, char *_file, size_t _line) {
-    TRACE(STREAM, "[MEMORY] CALL: malloc(%zu) in (%s:%zu)\n", size, _file, _line);
-    memory_chunk_t *chunk = __watchdog_allocate(size, _file, _line, false);
-    if (NULL == chunk) {
-        return NULL;
+    TRACE(STREAM, "[WATCHDOG] CALL: at (%s:%zu) -- malloc  : %zu bytes requested\n", _file, _line, size);
+    void *data = __watchdog_allocate(size, _file, _line, false);
+    if (data) {
+        fprintf(STREAM, "[WATCHDOG] DONE: at (%s:%zu) -- malloc  : %zu bytes allocated to address %p\n", _file, _line, size, data);
+    } else {
+        fprintf(STREAM, "[WATCHDOG] FAIL: at (%s:%zu) -- malloc  : unable to allocate %zu bytes\n", _file, _line, size);
     }
-    xor_list_push_back(__memory, chunk);
-    return chunk + 1;
+    return data;
 }
 
 void *watchdog_calloc(size_t num, size_t size, char *_file, size_t _line) {
-    TRACE(STREAM, "[MEMORY] CALL: calloc(%zu, %zu) in (%s:%zu)\n", num, size, _file, _line);
-    memory_chunk_t *chunk = __watchdog_allocate(num * size, _file, _line, true);
-    if (NULL == chunk) {
-        return NULL;
+    const size_t actual_size = num * size;
+    TRACE(STREAM, "[WATCHDOG] CALL: at (%s:%zu) -- calloc  : %zu bytes requested\n", _file, _line, actual_size);
+    void *data = __watchdog_allocate(actual_size, _file, _line, true);
+    if (data) {
+        fprintf(STREAM, "[WATCHDOG] DONE: at (%s:%zu) -- calloc  : %zu bytes allocated to address %p\n", _file, _line, actual_size, data);
+    } else {
+        fprintf(STREAM, "[WATCHDOG] FAIL: at (%s:%zu) -- calloc  : unable to allocate %zu bytes\n", _file, _line, actual_size);
     }
-    xor_list_push_back(__memory, chunk);
-    return chunk + 1;
+    return data;
 }
 
 void *watchdog_realloc(void *ptr, size_t size, char *_file, size_t _line) {
-    TRACE(STREAM, "[MEMORY] CALL: realloc(%p, %zu) in (%s:%zu)\n", ptr, size, _file, _line);
-    memory_chunk_t *chunk = __watchdog_reallocate(ptr, size, _file, _line);
-    if (NULL == chunk) {
-        return NULL;
+    TRACE(STREAM, "[WATCHDOG] CALL: at (%s:%zu) -- realloc : %zu bytes requested\n", _file, _line, size);
+    void *data = __watchdog_reallocate(ptr, size, _file, _line);
+    if (data) {
+        fprintf(STREAM, "[WATCHDOG] DONE: at (%s:%zu) -- realloc : %zu bytes reallocated from address %p to address %p\n", _file, _line, size, ptr, data);
+    } else {
+        fprintf(STREAM, "[WATCHDOG] FAIL: at (%s:%zu) -- realloc : unable to allocate %zu bytes\n", _file, _line, size);
     }
-    return chunk + 1;
+    return data;
 }
 
 void watchdog_free(void *ptr, char *_file, size_t _line) {
-    TRACE(STREAM, "[MEMORY] CALL: free(%p) in (%s:%zu)\n", ptr, _file, _line);
+    TRACE(STREAM, "[WATCHDOG] CALL: at (%s:%zu) -- free    : address %p\n", _file, _line, ptr);
     __watchdog_free(ptr, _file, _line);
 }
 
 void watchdog_exit(int status, char *_file, size_t _line) {
-    TRACE(STREAM, "[MEMORY] CALL: exit(%d) in (%s:%zu)\n", status, _file, _line);
+    TRACE(STREAM, "[WATCHDOG] CALL: at (%s:%zu) -- exit    : status %d\n", _file, _line, status);
     exit(status);
 }
 
 void watchdog_abort(char *_file, size_t _line) {
-    TRACE(STREAM, "[MEMORY] CALL: abort() in (%s:%zu)\n", _file, _line);
+    TRACE(STREAM, "[WATCHDOG] CALL: at (%s:%zu) -- abort\n", _file, _line);
     abort();
 }
